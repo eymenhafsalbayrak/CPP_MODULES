2575c36617ffbb05c86ef4bee4e099f7eb7d1b80
Eymen Albayrak <eyms@Delta.local>
2024-04-26 20:21:30 +0300

    fix

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -7,16 +7,14 @@ template <typename T>
     T min(T x, T y){
         if(x < y)
 			return x;
-		else
-			return y;
+		return y;
     }
 
 template <typename T> 
     T max(T x, T y){
         if(x > y)
 			return x;
-		else 
-			return y;
+		return y;
     }
 
 template <typename T>
