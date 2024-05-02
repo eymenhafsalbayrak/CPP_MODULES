dd9a69d68363ca5b002025b2534929318a9e60c9
Eymen Albayrak <eyms@192.168.1.100>
2024-05-03 01:29:00 +0300

    iterator

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -9,23 +9,43 @@
 template<typename T>
 class MutantStack : public std::stack<T>{
 
-    public:
-        MutantStack(){};
-        MutantStack(const MutantStack& copy){
-            *this = copy;
-        };
-        MutantStack operator=(const MutantStack& copy){
-            *this->std::stack<T> = std::stack<T>(copy);
-        };
-        ~MutantStack(){};
+	public:
+		MutantStack(){};
+		MutantStack(const MutantStack& copy){
+			*this = copy;
+		};
+		MutantStack operator=(const MutantStack& copy){
+			*this->std::stack<T> = std::stack<T>(copy);
+		};
+		~MutantStack(){};
 
-        typedef typename std::deque<T>::iterator iterator;
-        iterator begin(){
-            return(this->c.begin());
-        };
-        iterator end(){
-            return (this->c.end());
-        }; 
+		// typedef typename std::deque<T>::iterator iterator;
+		// iterator begin(){
+		//     return(this->c.begin());
+		// }
+		// iterator end(){
+		//     return (this->c.end());
+		// }
+
+		// iterator
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin() { return this->c.begin(); }
+		iterator end() { return this->c.end(); }
+
+		//const iterator
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		const_iterator cbegin() const{return this->c.cbegin();}
+		const_iterator cend() const{return this->c.cend();}
+
+		//reverse iterator
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		reverse_iterator rbegin(){return this->c.rbegin();}
+		reverse_iterator rend(){return this->c.rend();}
+
+		//const reverse iterator
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+		const_reverse_iterator crbegin() const{return this->c.crbegin();}
+		const_reverse_iterator crend() const{return this->c.crend();}
 };
 
 //you have to make std::stack container iterable
