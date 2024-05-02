94b21052a74256e922a2253964d4f45f34a42908
Eymen Hafsa Albayrak <ealbayra@k2m04s01.42kocaeli.com.tr>
2024-05-02 20:22:13 +0300

    08-iterator

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,13 +1,31 @@
 #pragma once
 
 #include <iostream>
+#include <stack>
+#include <deque>
+#include <list>
 
 
-class MutantStack{
+template<typename T>
+class MutantStack : public std::stack<T>{
 
     public:
-        MutantStack();
-        MutantStack(const MutantStack& copy);
-        MutantStack operator=(const MutantStack& copy);
-        ~MutantStack();
+        MutantStack(){};
+        MutantStack(const MutantStack& copy){
+            *this = copy;
+        };
+        MutantStack operator=(const MutantStack& copy){
+            *this->std::stack<T> = std::stack<T>(copy);
+        };
+        ~MutantStack(){};
+
+        typedef typename std::deque<T>::iterator iterator;
+        iterator begin(){
+            return(this->c.begin());
+        };
+        iterator end(){
+            return (this->c.end());
+        }; 
 };
+
+//you have to make std::stack container iterable
